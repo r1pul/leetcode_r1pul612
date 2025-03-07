4b507d7fc8e79812cc04cf3c406203c4211cab1c
Ripul Gautam <103016035+r1pul@users.noreply.github.com>
2025-03-07 12:21:56 +0530

    Time: 10 ms (54.93%), Space: 11.5 MB (62.86%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size();
-        unordered_map<char, int> mp;
-        if (t.size() > n)
-            return "";
+        int n= s.length();
+        int j = 0, i = 0 ; 
+        if(t.length()>n)
+        return "";
+        int rc=t.length(), s_i = 0 , mw = INT_MAX;
 
-        for (auto& ch : t)
-            mp[ch]++;
+        unordered_map<char,int> mp;
+        for(auto& ch : t)
+        mp[ch]++;
 
-        int reqc = t.size();
-        int i = 0, j = 0;
-        int wsize = INT_MAX;
-        int s_i = 0;
+        while(j<n)
+        {
+            char curr = s[j];
+            if(mp[curr] > 0)
+            rc--;
 
-        while (j < n) {
-            char ch = s[j];
-            if (mp[ch] > 0)
-                reqc--;
-            mp[ch]--;
-
-            while (reqc == 0) {
-                int cc = j - i + 1;
-
-                if (wsize > cc) {
-                    wsize = cc;
-                    s_i = i;
+            mp[curr]--;
+            while(rc==0)
+            {
+                if(mw > j-i+1)
+                {
+                    mw=j-i+1;
+                    s_i= i ;
                 }
-                
-                mp[s[i]]++;
-                if (mp[s[i]] > 0)
-                    reqc++;
+                char ch_i = s[i];
+                mp[ch_i]++;
+                if(mp[ch_i]>0)
+                rc++;
 
                 i++;
             }
             j++;
+
         }
-        return wsize == INT_MAX ? "" : s.substr(s_i, wsize);
+        return mw == INT_MAX ? "" : s.substr(s_i,mw);
+
     }
 };
