184e4e8d73d6f76c232289ac8d6e80c632f73003
Ripul Gautam <103016035+r1pul@users.noreply.github.com>
2025-03-23 02:55:50 +0530

    Time: 16 ms (92.82%), Space: 117.8 MB (95.28%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,52 +1,74 @@
 class Solution {
 public:
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        unordered_map<int,vector<int>> mp ;
-        for(auto &vec:edges){
-            int u = vec[0];
-            int v = vec[1];
-            mp[u].push_back(v);
-            mp[v].push_back(u);
+    class DSU {
+        public:
+        vector<int> parrent;
+        vector<int> Size;
+        DSU(int n ){
+            parrent.resize(n);
+            Size.resize(n);
+            for(int i = 0 ; i < n ;  i++)
+            {
+                parrent[i]=i;
+                Size[i]=1;
+            }
 
         }
-        int res=0;
-        vector<bool>visited(n,false);
-        for(int i = 0 ; i < n ; i++)
+        int find(int x) {
+            // Path compression: make the tree flatter
+            if (parrent[x] == x) {
+                return x;  // Recursively find the root and update the parent
+            }
+            return parrent[x]=find(parrent[x]);
+        }
+        void Union(int x , int y)
         {
-            if(visited[i]==true)
-            continue;
+            int x_p = find(x);
+            int y_p = find(y);
+            if(x_p==y_p)
+            return;
+            if(Size[x_p] > Size[y_p]){
+                parrent[y_p]=x_p;
+                Size[x_p]+=Size[y_p];
+            }
+            else if(Size[x_p] < Size[y_p]){
+                parrent[x_p]= y_p;
+                Size[y_p]+=Size[x_p];
+            }
+            else{
+            parrent[y_p]= x_p;
+            Size[x_p]+=Size[y_p];
+                }
 
-            int u = 0 , v = 0;
-            bfs(mp,visited,u,v,i);
-            if((u*(u-1))==v)
-            res++;
+        }
+
+
+    };
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        DSU dsu(n);
+        unordered_map<int,int>mp;
+        for(auto &vec : edges){
+            int u = vec[0];
+            int v = vec[1];
+            dsu.Union(u,v);
+        }
+        for(auto &vec : edges){
+            int u = vec[0];
+            int v = vec[1];
+            int root = dsu.find(u);
+            mp[root]++;
+        }
+        int res= 0;
+        for(int i = 0 ; i < n ; i++){
+            if(dsu.find(i)==i)
+            {
+                int u = dsu.Size[i];
+                int v = mp[i];
+                if ((u * (u - 1)) / 2 == v)
+                res++;
+            }
         }
         return res;
 
-
-
-
     }
-    void bfs(unordered_map<int,vector<int>> &mp , vector<bool>& visited , int &u , int &v, int i){
-        queue<int>que;
-        que.push(i);
-        visited[i]=true;
-
-        
-        while(!que.empty())
-        {
-            int c = que.front();
-            que.pop();
-            u++;
-            v += mp[c].size();
-            for(auto &vec : mp[c])
-            if(visited[vec]!=true)
-            bfs(mp,visited,u,v,vec);
-
-        }
-        
-        
-
-
-    }       
 };
