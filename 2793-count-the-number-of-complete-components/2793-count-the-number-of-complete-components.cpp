7f0f3345a1ab5c66b8592215a3e69c4003b19a8f
Ripul Gautam <103016035+r1pul@users.noreply.github.com>
2025-03-23 02:02:26 +0530

    Time: 116 ms (12.84%), Space: 182.1 MB (5.1%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,33 +1,52 @@
 class Solution {
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        unordered_map<int, vector<int>> mp;
-        for (auto& vec : edges) {
+        unordered_map<int,vector<int>> mp ;
+        for(auto &vec:edges){
             int u = vec[0];
             int v = vec[1];
             mp[u].push_back(v);
             mp[v].push_back(u);
-        }
-        int res = 0;
-        vector<bool> visited(n, false);
-        for (int i = 0; i < n; i++) {
-            if (visited[i]==true)
-                continue;
 
-            int u = 0, v = 0;
-            dfs(mp, visited, u, v, i);
-            if ((u * (u - 1)) == v)
-                res++;
+        }
+        int res=0;
+        vector<bool>visited(n,false);
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(visited[i]==true)
+            continue;
+
+            int u = 0 , v = 0;
+            bfs(mp,visited,u,v,i);
+            if((u*(u-1))==v)
+            res++;
         }
         return res;
+
+
+
+
     }
-    void dfs(unordered_map<int, vector<int>>& mp, vector<bool>& visited, int& u,
-             int& v, int i) {
-        visited[i] = true;
-        u++;
-        v += mp[i].size();
-        for (auto& n : mp[i])
-            if (visited[n] != true)
-                dfs(mp, visited, u, v, n);
-    }
+    void bfs(unordered_map<int,vector<int>> &mp , vector<bool>& visited , int &u , int &v, int i){
+        queue<int>que;
+        que.push(i);
+        visited[i]=true;
+
+        
+        while(!que.empty())
+        {
+            int c = que.front();
+            que.pop();
+            u++;
+            v += mp[c].size();
+            for(auto &vec : mp[c])
+            if(visited[vec]!=true)
+            bfs(mp,visited,u,v,vec);
+
+        }
+        
+        
+
+
+    }       
 };
