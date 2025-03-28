01e207857f61dd2c532ab2d2e9d2d5db018d402c
Ripul Gautam <103016035+r1pul@users.noreply.github.com>
2025-03-28 11:41:26 +0530

    Time: 3 ms (58.22%), Space: 7.5 MB (100%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,28 +1,37 @@
 class Solution {
-public:
-    int t[1001][1001];
+    public :
     int minSteps(int n) {
         if (n == 1)
             return 0;
-
-        memset(t, -1, sizeof(t));
-        return 1 + solve(1, 1, n);
-    }
-    int solve(int curr, int clip, int n) {
-        if (curr == n) {
-            return 0;
+        
+        /*
+            if remaining A's is divisible by # A's we have right now,
+            we straight away copy and paste entire A's we have greedily
+            Suppose ,
+            current   = 'AAA'
+            remaining = 'AAAAAA'
+            remaining%curr == 0
+            so, we copy and paste straight away : 'AAA'
+            current becomes 'AAAAAA'
+        */
+        
+        int remaining = n; // initially
+        int buffer = 0;
+        int count_A = 1; // initially we have 1 'A'
+        int steps = 0;
+        
+        while (count_A < n) {
+            remaining = n - count_A;
+            if (remaining % count_A == 0) {
+                buffer = count_A;
+                count_A *= 2;
+                steps += 2; // copy + paste
+            } else {
+                count_A += buffer;
+                steps++;
+            }
         }
-
-        if (curr > n) {
-            return 1000;
-        }
-        if (t[curr][clip] != -1) {
-            return t[curr][clip];
-        }
-
-        int copyallpaste = 1 + 1 + solve(curr + curr, curr, n);
-        int paste = 1 + solve(curr+clip, clip, n);
-
-        return t[curr][clip] = min(copyallpaste, paste);
+        
+        return steps;
     }
 };
