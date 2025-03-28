0f898cb12d4458d4a54d235f0126c145126bb09a
Ripul Gautam <103016035+r1pul@users.noreply.github.com>
2025-03-28 11:42:28 +0530

    Time: 6 ms (54.1%), Space: 9.6 MB (97.43%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> arr(n );
+        vector<int> arr(n+1 );
         int i2, i3, i5;
-        arr[0] = 1;
-        i2 = i3 = i5 = 0;
-        for (int i = 1; i < n; i++) {
+        arr[1] = 1;
+        i2 = i3 = i5 = 1;
+        for (int i = 2; i <= n; i++) {
             int u2 = arr[i2] * 2;
             int u3 = arr[i3] * 3;
             int u5 = arr[i5] * 5;
@@ -20,6 +20,6 @@ public:
             if (uMin == u5)
                 i5++;
         }
-        return arr[n-1];
+        return arr[n];
     }
 };
